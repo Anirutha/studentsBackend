86dcd97668ce52ae1e6daebec098d009f82bd8cf
Anirutha <aniruthasivakumar03@gmail.com>
2023-05-31 18:29:05 +0530

    new commit added

---
+++ Controllers/students.js
@@ -0,0 +1,40 @@
+import { ObjectId } from "bson";
+import { client } from "../db.js";
+
+
+export function getAllStudents(req){
+    return client
+    .db("b45wd")
+    .collection("students")
+    .find(req.query)
+    .toArray();
+}
+
+export function getStudentsById(id){
+    return client
+    .db("b45wd")
+    .collection("students")
+    .findOne({_id: new ObjectId(id)})
+}
+
+export function addStudentsData(data){
+  return client
+  .db("b45wd")
+  .collection("students")
+  .insertOne(data)
+
+}
+
+export function updateStudentData(id, updatedData){
+    return client
+    .db("b45wd")
+    .collection("students")
+    .findOneAndUpdate({_id: new ObjectId(id)},{$set:updatedData})
+}
+
+export function deletaStudentsData(id){
+    return client
+  .db("b45wd")
+  .collection("students")
+  .deleteOne({_id: new ObjectId(id)})
+}
