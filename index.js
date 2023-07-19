3b10c1ff4185475e22d4bd9dfd877d680a23dd54
Anirutha <aniruthasivakumar03@gmail.com>
2023-07-19 08:06:20 +0530

    all changes done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import { userRouter } from "./Routers/users.js";
 import cors from "cors"
+import { isAuthenticated } from "./Authentication/auth.js";
+import { studentsRouter } from "./Routers/students.js";
 
 //confgure the environment
 dotenv.config();
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 //routers
+app.use("/students",isAuthenticated,studentsRouter)
 app.use("/users",userRouter )
 //starting the server
 app.listen(PORT, ()=>console.log("server running in localhost:9090"))
